4e6e73f9b1e689041166a2945db3658937bd75af
SailajaPeram <60641644+SailajaPeram@users.noreply.github.com>
2022-11-12 12:51:17 +0530

    Update CiCd_script.py

--- CiCd_script.py
+++ CiCd_script.py
@@ -2,7 +2,8 @@ print("abc")
 input="Hello world"
 print(input)
 k=input.split()
-if(k[0].lower() == "hello"):
+first_word=k[0].lower()
+if(first_word == "hello"):
   print("Success")
 else:
   print("Fail")
