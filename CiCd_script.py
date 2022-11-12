e73752a9f2b35a526b69df340a15a0f15ea6308a
SailajaPeram <60641644+SailajaPeram@users.noreply.github.com>
2022-11-12 12:36:05 +0530

    Update CiCd_script.py

--- CiCd_script.py
+++ CiCd_script.py
@@ -1,16 +1,1 @@
-import os
-class HelloWorld:
-    def validate(self):
-        print("1")
-        self.input=os.environ["input"]
-        print("2")
-        k = self.input.split()
-        if (k[0].lower() == "hello"):
-            print("success")
-        else:
-            print("fail")
-
-obj=HelloWorld()
-obj.validate()
-
-
+print("abc")
