10e37d6ec92abb99159bdab82681b232d1dbffde
SailajaPeram <60641644+SailajaPeram@users.noreply.github.com>
2022-11-12 12:34:48 +0530

    Update CiCd_script.py

--- CiCd_script.py
+++ CiCd_script.py
@@ -1,9 +1,8 @@
 import os
 class HelloWorld:
     def validate(self):
-        self.input = "Hello How are you"
         print("1")
-        # self.input=os.environ.get("Enter the input")
+        self.input=os.environ["input"]
         print("2")
         k = self.input.split()
         if (k[0].lower() == "hello"):
